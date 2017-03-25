2be83d79954cbf74c732e5349fd0d3c0e2f0852e
Christopher Williams <christopher.s.williams@outlook.com>
2017-03-25 13:16:04 -0700

    GUI project now displays GPU data

--- NVOverclocker/MainWindow.xaml.cs
+++ NVOverclocker/MainWindow.xaml.cs
@@ -13,16 +13,49 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using MahApps.Metro.Controls;
+
+using NVOverclocker.Util;
+
 namespace NVOverclocker
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
+        private int numGPUs;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            numGPUs = NvidiaUtil.GetNumberOfGPUs();
+
+            if (numGPUs <= 0)
+            {
+                MessageBox.Show("Could not get GPU data!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                var gpuInfo = new GPUInfo();
+                if (NvidiaUtil.GetGPUInfo((UInt32)numGPUs, ref gpuInfo))
+                {
+                    Label_GPU_Name_Text.Content = gpuInfo.name;
+                    Label_GPU_Bios_Text.Content = gpuInfo.bios;
+                    Label_GPU_vRam_Text.Content = (gpuInfo.ramSize / 1024) + "MB " + gpuInfo.ramType;
+                    Label_GPU_CoreClock_Text.Content = (gpuInfo.currentCoreClock / 1000) + "MHz";
+                    Label_GPU_vRamClock_Text.Content = (gpuInfo.currentRamClock / 1000) + "MHz";
+                    Label_GPU_CoreOverclock_Text.Content = (gpuInfo.currentCoreOverclock / 1000) + "MHz";
+                    Label_GPU_vRamOverclock_Text.Content = (gpuInfo.currentRamOverclock / 1000) + "MHz";
+                }
+                else
+                {
+                    MessageBox.Show("Could not get info for GPU!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    Application.Current.Shutdown();
+                }
+            }
         }
     }
 }
---
+++ NVOverclocker/Util/NvidiaUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NVOverclocker.Util
+{
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct GPUInfo
+    {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+        public String name;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+        public String bios;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 6)]
+        public String ramType;
+        public Int32 ramSize;
+        public UInt32 currentCoreClock;
+        public UInt32 currentRamClock;
+        public Int32 currentCoreOverclock;
+        public Int32 currentRamOverclock;
+    }
+
+    public class NvidiaUtil
+    {
+        [DllImport("NVAPIWrapper.dll")]
+        public static extern Int32 GetNumberOfGPUs();
+
+        [DllImport("NVAPIWrapper.dll", CharSet = CharSet.Unicode)]
+        public static extern bool GetGPUInfo([In] UInt32 gpuIndex, [In, Out] ref GPUInfo gpuInfo);
+    }
+}
